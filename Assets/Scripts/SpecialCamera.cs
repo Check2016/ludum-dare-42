99d5e6a14ccb9927ba87846301770656495c5859
Check2016 <info@marcel-egle.de>
2018-08-11 11:28:42 +0200

    Prototyping with Camera

---
+++ Assets/Scripts/SpecialCamera.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class SpecialCamera : MonoBehaviour {
+
+    public Camera FPSCamera;
+    public float SwayAmount = 1;
+    public float SwaySpeed = 1;
+
+    public Animator animator;
+
+    private Vector3 startPosition;
+
+    private void Start()
+    {
+        startPosition = transform.localPosition;
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+
+
+        float mouseX = CrossPlatformInputManager.GetAxis( "Mouse X" );
+        float mouseY = CrossPlatformInputManager.GetAxis( "Mouse Y" );
+
+        transform.localPosition += new Vector3( mouseX, mouseY, 0 ) * SwayAmount;
+
+        transform.localPosition = Vector3.Lerp( transform.localPosition, startPosition, Time.deltaTime * SwaySpeed );
+    }
+}
